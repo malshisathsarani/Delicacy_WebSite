26233424938f3d65e169ed48b20fb858d988be94
Malshi <malshisathsarani1@gmail.com>
2024-10-19 12:26:16 +0530

    Initial commit

---
+++ Fruits&Vegetables.js
@@ -0,0 +1,10 @@
+//Swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,  // Enables continuous looping
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
